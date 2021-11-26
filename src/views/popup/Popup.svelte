2fd58e398cbd3ab8b09e619e709a0234d077e8d1
louismeunier <louismeunier123@gmail.com>
2021-11-26 10:33:11 -0500

    Views

---
+++ src/views/popup/Popup.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    async function handleClick() {
+        chrome.runtime.sendMessage({message: "backup"}, () => {})
+    }
+</script>
+
+<style>
+    div {
+        color: red;
+    }
+</style>
+
+<div>
+    <button on:click="{handleClick}">Backup</button>
+</div>
